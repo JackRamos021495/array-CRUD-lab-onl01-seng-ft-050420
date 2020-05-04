ab779ded0720cbdd07ab78d4b35e166fab71f560
Jackson Ramos <jackramos021495@gmail.com>
2020-05-04 20:56:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-array.unshift
+element = array.unshift
 end
 
 def remove_element_from_end_of_array(array)
@@ -19,7 +19,7 @@ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
